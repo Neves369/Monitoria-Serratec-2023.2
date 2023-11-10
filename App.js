1e03b8584c6831feed51866a06dea03caead9fb6
Neves369 <douglasbrian369@hotmail.com>
2023-11-10 18:25:19 -0300

    atualizações

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Image, TouchableOpacity } from 'react-native';
+import logo from "./assets/logo.png";
 
 export default function App() {
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const enviar = () =>{
+    console.log("email: ", email);
+    console.log("senha: ", senha);
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+
+      <Image resizeMode="contain" style={styles.logo} source={logo}/>
+
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Endereço de e-mail"
+        value={email}
+        keyboardType="email-address"
+        onChangeText={(e)=>{setEmail(e)}}
+      />
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Senha"
+        value={senha}
+        onChangeText={(valor)=>{setSenha(valor)}}
+        secureTextEntry={true}
+      />
+
+      {/* <Button 
+        title='ENTRAR'
+        onPress={()=>{}}
+      /> */}
+
+      <TouchableOpacity style={styles.button} onPress={()=>{enviar()}}>
+        <Text style={styles.textButton}>ENTRAR</Text>
+      </TouchableOpacity>
+
+
     </View>
   );
 }
@@ -16,5 +58,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 25
   },
+  logo:  {
+    height: 100,
+    width: 100,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold" 
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+  },
+  button: {
+    padding: 12,
+    width: 300,
+    backgroundColor: "#4accf0",
+    alignItems: "center",
+    borderRadius:5
+  },
+  textButton: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold"
+  }
 });
