cd582e8b6db029fbf19d6fb3c53860762a172259
Neves369 <douglasbrian369@hotmail.com>
2023-11-13 17:59:08 -0300

    Finalização da monitoria 2 com flatlist

--- 2-lista-de-contatos/App.js
+++ 2-lista-de-contatos/App.js
@@ -1,96 +1,32 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, ScrollView, FlatList} from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList} from "react-native";
 import contatos from "./contatos.json"
 
 const Contact = ({ name, phone }) => {
   return (
-   <></>
+    <View style={styles.contact}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.phone}>{phone}</Text>
+    </View>
   );
 };
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    {
-      "nome": "Cecília Analu Joana Cavalcanti",
-      "celular": "(91) 99274-4656",
-      
-    },
-    {
-      "nome": "Vinicius Miguel Gabriel Nunes",
-      "celular": "(41) 99205-4756",
-     
-    },
-    {
-      "nome": "Emily Isabel Teixeira",
-      "celular": "(61) 98240-5664",
-      
-    },
-    {
-      "nome": "José Nathan Heitor Aparício",
-      "celular": "(63) 98575-7169",
-     
-    },
-    {
-      "nome": "Kaique Kaique Manuel Oliveira",
-      "celular": "(68) 98934-8243",
-      
-    },
-    {
-      "nome": "Renato Lorenzo Lucas Bernardes",
-      "celular": "(71) 98640-1437",
-      
-    },
-    {
-      "nome": "Noah Cauã Gonçalves",
-      "celular": "(95) 99619-9594",
-      
-    },
-    {
-      "nome": "Levi Luiz Vinicius Costa",
-      "celular": "(96) 98912-4148",
-      
-    },
-    {
-      "nome": "Bernardo Paulo Oliver Caldeira",
-      "celular": "(83) 98720-0946",
-      
-    },
-    {
-      "nome": "Enzo Sérgio Fogaça",
-      "celular": "(95) 98835-2271",
-      
-    },
-    {
-      "nome": "Rebeca Mariana Rebeca Ferreira",
-      "celular": "(95) 98603-9051",
-      
-    },
-    {
-      "nome": "João Emanuel Martins",
-      "celular": "(67) 99714-0824",
-     
-    },
-    {
-      "nome": "Sophie Alana Heloise Melo",
-      "celular": "(91) 99124-5377",
-      
-    },
-    {
-      "nome": "Oliver Paulo Jesus",
-      "celular": "(48) 98708-6386",
-      
-    },
-    {
-      "nome": "Pietro Roberto Joaquim Barbosa",
-      "celular": "(81) 98539-5160",
-      
-    }
-  ]);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    setContacts(contatos.contatos)
+  }, [])
 
   
   return (
     <View style={styles.container}>
-     
+      <FlatList
+        data={contacts}
+        ListEmptyComponent={()=><Text>Lista Vazia</Text>}
+        keyExtractor={(item) => item.nome}
+        renderItem={({ item }) => <Contact key={item.nome} name={item.nome} phone={item.celular} />}
+      />
     </View>
   );
 };
@@ -100,7 +36,19 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 50,
     backgroundColor: "#fff",
-  }
+  },
+  contact: {
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#ccc",
+  },
+  name: {
+    fontSize: 20,
+  },
+  phone: {
+    fontSize: 16,
+  },
 });
 
 export default App;
